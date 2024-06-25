886ad4594de37a596b571aad7f0a2c7788052b86
Aravind-Mohan-Sree <mailuraravindms@gmail.com>
2024-06-25 15:41:07 +0530

    ''

--- js-problems/find-repeated-words.js
+++ js-problems/find-repeated-words.js
@@ -1,20 +1,20 @@
-// find repeated words in a sentence and its occurance.
-const str = 'The kids played the game until the kids were too tired to play the game anymore';
+// find repeated words in a sentence and its intensity.
+const str = `The rain kept falling, and the rain didn't stop until the sun appeared and the rain was gone`;
 const strArr = str.toLowerCase().split(' ');
 const map = new Map();
 
-strArr.forEach((ele, ind, arr) => {
+strArr.forEach((ele) => {
   let count = 0;
 
-  for (let i = ind; i < arr.length; i++) {
-    if (arr.includes(ele, i)) {
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr.includes(ele, i)) {
       count++;
-      i = arr.indexOf(ele);
+      i = strArr.indexOf(ele, i);
     }
   }
 
   if (count > 1) {
-    map.set(ele, count--);
+    map.set(ele, --count);
   }
 });
 
