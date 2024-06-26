e1d824b8d83829aa3a951ea38be7020affa6600b
Aravind-Mohan-Sree <mailuraravindms@gmail.com>
2024-06-26 13:08:10 +0530

    ''

--- js-problems/find-repeated-words.js
+++ js-problems/find-repeated-words.js
@@ -3,3 +3,12 @@ const str = `The rain kept falling, and the rain didn't stop until the sun appea
 const strArr = str.toLowerCase().split(' ');
 const map = {};
 
+for (const ele of strArr) {
+  map[ele] = (map[ele] || 0) + 1;
+}
+
+for (const key in map) {
+  if (map[key] > 1) {
+    console.log(`${key} = ${map[key] - 1}`);
+  }
+}
