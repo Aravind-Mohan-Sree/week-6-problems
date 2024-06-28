8f73f32984333fd77fcbc81d95a37ef953f8b7b1
Aravind-Mohan-Sree <mailuraravindms@gmail.com>
2024-06-28 12:16:02 +0530

    ''

---
+++ js-problems/sort-according-to-vowels.js
@@ -0,0 +1,17 @@
+// sort words in an array on the basis of vowels intensity.
+let words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"];
+const countArr = [];
+const vowels = 'aeiouAEIOU';
+
+for (const ele of words) {
+  const count = ele.split('').filter((ele) => {return vowels.includes(ele)}).length;
+
+  countArr.push([ele, count]);
+}
+
+countArr.sort((a, b) => {return a[1] - b[1]});
+words.splice(0);
+
+words = countArr.map((ele) => {return ele[0]});
+
+console.log(words);
