04b621c025f7a83d8cb6354733ed4ae78b4710f9
Aravind-Mohan-Sree <mailuraravindms@gmail.com>
2024-06-29 14:28:46 +0530

    ''

---
+++ js-problems/sum-first-and-last-digit-of-a-number.js
@@ -0,0 +1,7 @@
+// sum first and last digit of a number.
+const num = 6135334515;
+const strArr = num.toString();
+
+const sum = + strArr[0] + + strArr[strArr.length - 1];
+
+console.log(sum);
--- js-problems/sum-first-and-last-digits-of-a-number.js
+++
@@ -1,7 +0,0 @@
-// sum first and last digit of a number.
-const num = 6135334515;
-const strArr = num.toString().split('');
-
-const sum = +strArr[0] + +strArr[strArr.length - 1];
-
-console.log(sum);
